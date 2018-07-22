Fixed equilibriation kinetic energy display, added total calculation timer

plot.py
@@ -1,25 +1,17 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import sys
-file = open(sys.argv[1])
-allData = file.read()
 
-split = allData.split('\n')
-
+data = open(sys.argv[1]).read()
 ys = []
 
-for i in range(0,len(split)):
-    entry = split[i].strip()
-    try:
-        ys.append(float(entry))
-    except:
-        continue
+for line in data.split('\n'):
+	try: 
+		ys.append(float(line))
+	except:
+		continue
 
+ys = np.array(ys)
+print ys.mean()
 plt.plot(ys)
-#plt.ylim(-0.6,-0.2)
-#plt.axes().set_yscale("log")
-#plt.xlim(0,10)
-#plt.xlabel("Bond length (Å)", fontsize=20)
-#plt.ylabel("$E_V$ (Hartree)", fontsize=20)
-#plt.tight_layout()
-#plt.axes().set_aspect('equal', 'datalim')
 plt.show()